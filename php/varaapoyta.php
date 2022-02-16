c84a845a75bb1aa1923c9df818afcce13a12d910
TanikaS <tanika21000@student.hamk.fi>
2022-02-16 11:46:55 +0200

    muokkaus

--- php/varaapoyta.php
+++ php/varaapoyta.php
@@ -1,13 +1,14 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 
+$id=isset($_POST["id"]) ? $_POST["id"] : "";
 $etunimi=isset($_POST["etunimi"]) ? $_POST["etunimi"] : "";
 $sukunimi=isset($_POST["sukunimi"]) ? $_POST["sukunimi"] : "";
 $sähköposti=isset($_POST["sähköposti"]) ? $_POST["sähköposti"] : "";
 $puhelinnumero=isset($_POST["puhelinnumero"]) ? $_POST["puhelinnumero"] : "";
 
 
-if (empty($etunimi) || empty($sukunimi) || empty($sähköposti) || empty($puhelinnumero)){
+if (empty ($id) ||  empty($etunimi) || empty($sukunimi) || empty($sähköposti) || empty($puhelinnumero)){
     header("Location:../html/varaapoyta.html");
     exit;
 }
@@ -23,12 +24,12 @@ try{
  }
 
 
-$sql="insert into varaapoyta (etunimi, sukunimi, sähköposti, puhelinnumero) values(?, ?, ?, ?)";
+$sql="insert into varaapoyta (etunimi, sukunimi, sähköposti, puhelinnumero, id) values(?, ?, ?, ?)";
 
 //Valmistellaan sql-lause
 $stmt=mysqli_prepare($yhteys, $sql);
 //Sijoitetaan muuttujat oikeisiin paikkoihin
-mysqli_stmt_bind_param($stmt, 'sdi', $etunimi, $sukunimi, $sähköposti, $puhelinnumero);
+mysqli_stmt_bind_param($stmt, 'sdi', $etunimi, $sukunimi, $sähköposti, $puhelinnumero, $id);
 //Suoritetaan sql-lause
 mysqli_stmt_execute($stmt);
 //Suljetaan tietokantayhteys
